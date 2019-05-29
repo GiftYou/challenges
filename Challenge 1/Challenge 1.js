11206f52b813cdf1e8dd19a8d3d82f208c3fad62
Saturnux <myusuf.alvikri@yahoo.com>
2019-05-29 17:14:06 +1200

    adding Challenge 1

---
+++ Challenge 1/Challenge 1.js
@@ -0,0 +1,39 @@
+function sum(){
+    // make first value;
+    var val = 0;
+    
+    // 
+    for(var i = 0; i < arguments.length; i++){
+        val += arguments[i];
+        
+    }
+    var res = console.log(val);
+    
+    return res;
+    
+    
+}
+
+sum(1,2,7);
+sum(1,4);
+sum(11);
+sum(10,3,6,7,9);
+
+// here are master branch
+
+// function sum(){
+//     var arrayy = [1,2,3,4,5];
+    
+//     for(var i = 0; i < arrayy.length; i++){
+//         console.log(arrayy);
+        
+        
+//     }
+    
+    
+// }
+
+// sum();
+
+
+// Arithmatic, Assignment, Function, Arguments
