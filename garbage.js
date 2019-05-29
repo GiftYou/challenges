cd1d74d90dc2233659abd0851e8995fe7b8477b4
Saturnux <myusuf.alvikri@yahoo.com>
2019-05-29 14:53:56 +1200

    gar

---
+++ garbage.js
@@ -0,0 +1,58 @@
+// function sum(a,b,c,d,e){
+//     var R = a + b + c + d +e;
+
+//     console.log(R);
+    
+// }
+
+// sum(1,2,7);
+// sum(1,4);
+// sum(11);
+// sum(10,3,6,7,9);
+
+// var a = 0;
+// var b = 0;
+// var c = 0;
+// var d = undefined;
+// var e = undefined;
+
+/* leave here */
+
+// function sum(a,b = 0,c = 0,d = 0,e = 0){
+    
+    
+//     var R = a + b + c + d + e;
+
+//     console.log(R);
+    
+// }
+
+// sum(1,2,7);
+// sum(1,4);
+// sum(11);
+// sum(10,3,6,7,9);
+
+
+
+// var n = [1,2,3,3,5];
+//     var ll = n.length;
+//     var go = 0
+    
+
+//     for (let num = 0; num < n.length; num++) {
+//         var go = num;
+        
+        
+//     }
+    
+
+//     return go;
+
+
+    /* check length of array */ 
+
+// if(arr.length > 50){
+//     arr
+// }else{
+//     return arr;
+// }
