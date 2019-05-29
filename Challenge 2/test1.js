e37540fe0b37ce76f320b30a6c517e265e698b06
Saturnux <myusuf.alvikri@yahoo.com>
2019-05-29 15:02:07 +1200

    test1.js

---
+++ Challenge 2/test1.js
@@ -0,0 +1,12 @@
+function setup(nn){
+    var peluru = [1,2,3,4,5,6];
+
+    for (let ii = 0; ii <= nn; ii++) {
+        var res = console.log(peluru[ii]);
+        
+    }
+
+    return res;
+}
+
+console.log(setup(40));
